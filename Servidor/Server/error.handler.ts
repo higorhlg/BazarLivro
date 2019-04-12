998b0475c127abbf0299a58b5c1df9ad368c7f6e
mariodouglasac <mdac.mario@gmail.com>
2019-04-11 22:06:19 -0300

    Correção de erros dos códigos http

--- Servidor/Server/error.handler.ts
+++ Servidor/Server/error.handler.ts
@@ -1,15 +1,26 @@
 import * as restify from 'restify'
 
 
-export const handleError = (req: restify.Request,resp:restify.Response
-                            ,err : any,done : any) =>{
-        console.log(err)
+export const handleError = (req: restify.Request, resp: restify.Response
+    , err: any, done: any) => {
+    console.log(err)
 
-        err.toJSON  = ()=>{
-            return {
-                message: err.message
-            }
+    err.toJSON = () => {
+        return {
+            message: err.message
         }
+    }
 
-        done()
+    switch(err.name){
+        case 'MongooseError':
+            if(err.code === 11000){
+                err.statusCode = 400
+            }
+            break
+        case 'ValidationError':
+            err.statusCode = 400
+            break
+    }
+
+    done()
 }
