ec45998c6dbd8d5cc3489688191e0ad7449a2bd5
mariodouglasac <mdac.mario@gmail.com>
2019-04-11 23:26:07 -0300

    pequena correção na instrução na instrução

--- Servidor/common/environment.ts
+++ Servidor/common/environment.ts
@@ -4,10 +4,10 @@ export const environment = {
         url: process.env.DB_URL || 'mongodb+srv://mario:mariod@bazarlivro-g8jqg.mongodb.net/bazarLivro?retryWrites=true'
                                     //url para acessar MongoDB
     /**Para conectar no banco via terminal use o seguinte comando 
-     * mongo "mongodb+srv://bazarlivro-g8jqg.mongodb.net/test" --username mario --password mariod
+     * mongo "mongodb+srv://bazarlivro-g8jqg.mongodb.net/bazarLivro" --username mario --password mariod
      * Para conectar no banco via Mongo Compass copie o seguinte string e mongo compass detecta 
      * automaticamente e configura a conexão autómatica
-     * mongodb+srv://mario:mariod@bazarlivro-g8jqg.mongodb.net/test
+     * mongodb+srv://mario:mariod@bazarlivro-g8jqg.mongodb.net/bazarLivro
      */
     },
     security: {
