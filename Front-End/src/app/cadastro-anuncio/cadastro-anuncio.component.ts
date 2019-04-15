00ae0098d18d418958b02f7e96554a2c6f3c03e0
higorhlg <higorhlg@hotmail.com>
2019-04-15 20:42:59 -0300

    front end

---
+++ Front-End/src/app/cadastro-anuncio/cadastro-anuncio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-anuncio',
+  templateUrl: './cadastro-anuncio.component.html',
+  styleUrls: ['./cadastro-anuncio.component.scss']
+})
+export class CadastroAnuncioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Front-End/src/app/editar-conta/editar-conta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-conta',
+  templateUrl: './editar-conta.component.html',
+  styleUrls: ['./editar-conta.component.scss']
+})
+export class EditarContaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
