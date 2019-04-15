6603926e39fe05ef17ab8c048f7f19046783d0ec
mariodouglasac <mdac.mario@gmail.com>
2019-04-15 18:07:54 -0300

    Retirado erro de tipagem

--- Servidor/common/model.router.ts
+++ Servidor/common/model.router.ts
@@ -5,24 +5,24 @@ export abstract class ModelRouter<D extends mongoose.Document> extends Router {
     constructor( protected model: mongoose.Model<D>) {
         super()
     }
-    findAll =  (req, resp, next)=>{
+    findAll =  (req: any, resp: any, next: any)=>{
         this.model.find().then(this.render(resp, next))
         .catch(next)
     }
-    findBayId = (req, resp, next)=>{
+    findBayId = (req: any, resp: any, next: any)=>{
         this.model.findById(req.params.id).then(this.render(resp, next)).catch(next)
     }
-    save =  (req, resp, next) => {
+    save =  (req: any, resp: any, next: any) => {
         let document = new this.model(req.body)
         document.save().then(this.render(resp, next)).catch(next)
         return next()
     }
-    update = (req, resp, next)=>{
+    update = (req: any, resp: any, next: any)=>{
             const op = {runValidators: true, new:true}
             this.model.findByIdAndUpdate(req.params.id, req.body, op).then(this.render(resp, next))
             .catch(next)
     }
-    delete = (req, resp, next)=>{
+    delete = (req: any, resp: any, next: any)=>{
         this.model.remove({_id:req.params.id}).exec().then((user:any)=>{
             if(user.n){
                 resp.send(204)
