00d08a0d99ac2a3f5c3ae7ff01f801662d523091
mariodouglasac <mdac.mario@gmail.com>
2019-04-16 16:55:47 -0300

    adicionado rotas do anuncio no array

--- Servidor/main.ts
+++ Servidor/main.ts
@@ -1,11 +1,16 @@
 import { Server } from "./Server/server";
 import { usersRouter } from "./Classes/Usuario/usuario.router";
 import { announcementRouter } from "./Classes/Anuncio/anuncio.router";
+import { transactionsRouter } from "./Classes/Transacao/transacao.router";
 
 
 const server:Server = new Server
 
-server.bootstrap([usersRouter, announcementRouter]).then( server=> {
+server.bootstrap([
+    usersRouter,
+    announcementRouter,
+    transactionsRouter
+]).then( server=> {
     
     console.log(`servidor executando na porta ${server.application.address().port}`)
 }).catch( error=>{
