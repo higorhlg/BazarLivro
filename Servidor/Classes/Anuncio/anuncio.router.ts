05359f1a9ba30be3388c812b789ccfa34f02b815
mariodouglasac <mdac.mario@gmail.com>
2019-04-22 17:27:21 -0300

    Validação do id na criação do anuncio e transacao

--- Servidor/Classes/Anuncio/anuncio.router.ts
+++ Servidor/Classes/Anuncio/anuncio.router.ts
@@ -3,6 +3,8 @@ import * as mongoose from 'mongoose'
 import { ModelRouter } from '../../common/model.router';
 import { Announcement } from './anuncio.model';
 import { authorize, authorizeNoProfile } from '../../security/authz.handler';
+import { User } from '../Usuario/usuario.model';
+import { NotFoundError } from 'restify-errors';
 
 class AnnouncementRouter extends ModelRouter<Announcement>{
     constructor(){
@@ -19,11 +21,24 @@ class AnnouncementRouter extends ModelRouter<Announcement>{
         return query.populate('user', ['nome', 'email'])
     }
 
+    validateId = (req:restify.Request, resp: restify.Response, next:restify.Next)=>{
+        console.log('Entrou')
+        User.findById({_id: req.body.user}).then(user=>{
+            if(user){
+                console.log('Usuário ok')
+                next()
+            }
+            else{
+                next(new NotFoundError('Usuário não encontrado'))
+            }
+        }).catch(next)
+    }
+
     applyRouter(application: restify.Server){
         
         application.get('/announcements', authorizeNoProfile(), this.findAll)
         application.get('/announcements/:id', authorizeNoProfile(), [this.validateId, this.findById])
-        application.post('/announcements', authorizeNoProfile(), this.save)
+        application.post('/announcements', authorizeNoProfile(), [this.validateId, this.save])
         application.patch('/announcements/:id', authorizeNoProfile(), [this.validateId, this.update])
         application.del('/announcements/:id', authorizeNoProfile(), [this.validateId, this.delete])
     }
