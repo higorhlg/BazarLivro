2af7d4dda4f004a4b55752584851eb9a3df8d36b
higorhlg <higorhlg@hotmail.com>
2019-04-24 15:25:00 -0300

    usuario service

---
+++ Front-End/model/usuario.model.ts
@@ -0,0 +1,17 @@
+import { UsuarioService } from 'src/app/usuario.service';
+
+export class User{
+
+    _id:object
+    nome :string
+    dataNascimento:string
+    cpf:string
+    endereco:string
+    usuario:string
+    senha: any | string
+    email:string
+    telefone:string
+    profile?:string[]
+
+ 
+}
