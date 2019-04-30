78e9c6c686edcdaf86f9d5d7ebee169e3323d93c
Sergio Souza Novak <serginhosnovak@hotmail.com>
2019-04-30 11:23:19 -0300

    arrumado navbar

--- Front-End/src/app/tela-principal/carrossel-ofertas-quentes/carrossel-ofertas-quentes.component.ts
+++ Front-End/src/app/tela-principal/carrossel-ofertas-quentes/carrossel-ofertas-quentes.component.ts
@@ -6,6 +6,91 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrossel-ofertas-quentes.component.scss']
 })
 export class CarrosselOfertasQuentesComponent implements OnInit {
+  linhaCarrosselInicial = [{
+    src: "../../../assets/img/1.png",
+    descricao: "Harry Porco",
+    preco: "R$199.99"
+  },
+  {
+    src: "../../../assets/img/3.png",
+    descricao: "A corna",
+    preco: "R$199.99"
+  },
+  {
+    src: "../../../assets/img/1.png",
+    descricao: "Harry Porco",
+    preco: "R$199.99"
+  },
+  {
+    src: "../../../assets/img/3.png",
+    descricao: "A corna",
+    preco: "R$199.99"
+  },
+
+
+
+
+
+]
+
+
+
+linhasCarrossel =[
+  [{
+    src: "../../../assets/img/1.png",
+    descricao: "Harry Porco",
+    preco: "R$199.99"
+  },
+  {
+    src: "../../../assets/img/3.png",
+    descricao: "A corna",
+    preco: "R$199.99"
+  },
+  {
+    src: "../../../assets/img/1.png",
+    descricao: "Harry Porco",
+    preco: "R$199.99"
+  },
+  {
+    src: "../../../assets/img/3.png",
+    descricao: "A corna",
+    preco: "R$199.99"
+  },
+
+
+
+
+
+],
+[{
+  src: "../../../assets/img/1.png",
+  descricao: "Harry Porco",
+  preco: "R$199.99"
+},
+{
+  src: "../../../assets/img/3.png",
+  descricao: "A corna",
+  preco: "R$199.99"
+},
+{
+  src: "../../../assets/img/1.png",
+  descricao: "Harry Porco",
+  preco: "R$199.99"
+},
+{
+  src: "../../../assets/img/3.png",
+  descricao: "A corna",
+  preco: "R$199.99"
+},
+
+
+
+
+
+]
+]
+
+
 
   constructor() { }
 
