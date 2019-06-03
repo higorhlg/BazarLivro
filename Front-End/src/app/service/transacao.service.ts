7ef2e3f8e7016a87dcc506ef7919233ea047915a
higorhlg <higorhlg@hotmail.com>
2019-06-03 16:12:18 -0300

    servico e model transacao

---
+++ Front-End/src/app/service/transacao.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {Transacao} from 'model/transacao.model';
+import { HttpClient } from '@angular/common/http';
+import { api } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransacaoService {
+
+  constructor(private http: HttpClient) { }
+
+  public save(transacao: Transacao): Observable<Transacao> {
+    return this.http.post<Transacao>(`${api.url}/transactions`, transacao);
+  }
+  public getAll(): Observable<Transacao[]> {
+    return this.http.get<Transacao[]>(`${api.url}/transactions`);
+  }
+  public update(transacao: Transacao): Observable<Transacao> {
+    return this.http.patch<Transacao>(`${api.url}/transactions/${transacao._id}`, transacao);
+  }
+  public delete(transacao: Transacao): Observable<any> {
+    return this.http.delete<Transacao>(`${api.url}/transactions/${transacao._id}`,);
+  }
+
+}
