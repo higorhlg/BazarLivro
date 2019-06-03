12d914a3e620b99c6a752c3e6991c63a54d2bd7d
alunorv <sebastiaohndrv@gmail.com>
2019-06-03 17:00:04 -0300

    alterado usuario service

--- Front-End/src/app/service/usuario.service.ts
+++ Front-End/src/app/service/usuario.service.ts
@@ -13,4 +13,18 @@ export class UsuarioService {
   public save(user: User): Observable<User> {
     return this.http.post<User>(`${api.url}/users`, user);
   }
+
+  public getAll(): Observable<User[]> {
+    return this.http.get<User[]>(`${api.url}/users`);
+  }
+
+  public update(user: User): Observable<User>{
+    return  this.http.patch<User>(`${api.url}/users/${user._id}`, user);
+  }
+
+  public delete(user: User): Observable<any> {
+    return this.http.delete<User>(`${api.url}/users/${user._id}`,);
+  }
+
 }
+ 	
