b39ce4b93628ff9279e5d7e359e9f6b6e6a3ccf7
higorhlg <higorhlg@hotmail.com>
2019-06-03 17:21:48 -0300

    aa

--- Front-End/src/app/app.component.ts
+++ Front-End/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'BazarLivros';
+  navbar = true;
 }
