0f52c0e1f93475eadbf30ed2ca507ff0df8d500f
higorhlg <higorhlg@hotmail.com>
2019-06-05 15:49:06 -0300

    Alteração de dados

--- Front-End/src/app/service/usuario.service.ts
+++ Front-End/src/app/service/usuario.service.ts
@@ -3,6 +3,7 @@ import { User } from 'model/usuario.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { api } from 'src/environments/environment';
+import { HttpXsrfCookieExtractor } from '@angular/common/http/src/xsrf';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,13 @@ export class UsuarioService {
   public getAll(): Observable<User[]> {
     return this.http.get<User[]>(`${api.url}/users`);
   }
+  public getById(id : string) : Observable<User> {
+    return this.http.get<User>(`${api.url}/users/${id}`);
+  }
 
+  public getByUser(_id : string) : Observable<User> {
+    return this.http.get<User>(`${api.url}/users/${_id}`);
+  }
   public update(user: User): Observable<User>{
     return  this.http.patch<User>(`${api.url}/users/${user._id}`, user);
   }
